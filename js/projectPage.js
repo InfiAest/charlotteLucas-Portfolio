31720de55432b2dc22d654514f3de0cc2861b1ba
Charlotte Lucas <charlotte.lucas@hotmail.com>
2022-03-08 17:19:39 +0100

    layout and content changed

--- js/projectPage.js
+++ js/projectPage.js
@@ -1,5 +1,7 @@
 import getBaseColours from "./components/baseColours.js";
 import renderHamburgerNav from "./components/nav.js";
+import showOnScrollAnimation from "./components/showOnScrollAnimation.js";
 
 getBaseColours();
 renderHamburgerNav();
+showOnScrollAnimation();
