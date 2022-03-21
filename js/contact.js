c5c08971accefb43cf3a741ebfb1fd268a311e42
Charlotte Lucas <charlotte.lucas@hotmail.com>
2022-03-21 16:58:16 +0100

    .

--- js/contact.js
+++ js/contact.js
@@ -1,5 +1,3 @@
-import getBaseColours from "./components/baseColours.js";
 import renderHamburgerNav from "./components/nav.js";
 
-getBaseColours();
 renderHamburgerNav();
--- js/projectPage.js
+++ js/projectPage.js
@@ -1,7 +1,5 @@
-import getBaseColours from "./components/baseColours.js";
 import renderHamburgerNav from "./components/nav.js";
 import showOnScrollAnimation from "./components/showOnScrollAnimation.js";
 
-getBaseColours();
 renderHamburgerNav();
 showOnScrollAnimation();
