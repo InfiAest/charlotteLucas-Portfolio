18acfabe04e20f20a19bb7f886f2d47734659999
Charlotte Lucas <charlotte.lucas@hotmail.com>
2022-03-25 20:25:15 +0100

    styling

--- js/components/nav.js
+++ js/components/nav.js
@@ -1,30 +1,32 @@
 export default function renderHamburgerNav() {
-    const hamburgerButton = document.querySelector(".hamburgerButton");
+  const hamburgerButton = document.querySelector(".hamburgerButton");
 
-    hamburgerButton.addEventListener("click", toggleMenu);
+  hamburgerButton.addEventListener("click", toggleMenu);
 
-    const hamburgerMenu = document.querySelector(".hamburgerNav");
-    
-    function toggleMenu() {
-        if(hamburgerMenu.style.transform === "translateX(0%)" && window.innerWidth < 785) {
-            hamburgerMenu.style.transform = "translateX(-100%)";
-        }
-        else {
-            hamburgerMenu.style.transform = "translateX(0%)";
-        }
+  const hamburgerMenu = document.querySelector(".hamburgerNav");
+
+  function toggleMenu() {
+    if (
+      hamburgerMenu.style.transform === "translateX(0%)" &&
+      window.innerWidth < 785
+    ) {
+      hamburgerMenu.style.transform = "translateX(-100%)";
+    } else {
+      hamburgerMenu.style.transform = "translateX(0%)";
     }
+  }
 
-    window.onscroll = closeHamburgerNav();
+  window.onscroll = closeHamburgerNav();
 
-    function closeHamburgerNav() {
-        if(hamburgerMenu.style.transform = "translateX(0%)") {
-            hamburgerMenu.style.transform = "translateX(-100%)";
-        }
+  function closeHamburgerNav() {
+    if ((hamburgerMenu.style.transform = "translateX(0%)")) {
+      hamburgerMenu.style.transform = "translateX(-100%)";
     }
+  }
 
-    const menuButtons = document.querySelectorAll(".hamburgerNav a");
+  const menuButtons = document.querySelectorAll(".hamburgerNav a");
 
-    menuButtons.forEach((link) => {
-        link.onclick = closeHamburgerNav();
-    });
+  menuButtons.forEach((link) => {
+    link.onclick = toggleMenu();
+  });
 }
