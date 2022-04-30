4693168a41dd76704fcb9f8793964f0de420150f
Charlotte Lucas <charlotte.lucas@hotmail.com>
2022-04-30 18:41:24 +0200

    mobile navigation layout changed

--- js/components/nav.js
+++ js/components/nav.js
@@ -1,30 +1,51 @@
 export default function renderHamburgerNav() {
   const hamburgerButton = document.querySelector(".hamburgerButton");
+  const navbarColour = document.querySelector(".navbar");
 
   hamburgerButton.addEventListener("click", toggleMenu);
 
-  const hamburgerMenu = document.querySelector(".hamburgerNav");
+  const navbar = document.querySelector(".navbar");
+
+  console.log(window.location.pathname);
+
+  function setNavColour() {
+    if (window.location.pathname.includes("work")) {
+      navbarColour.style.backgroundColor = "#373737";
+    } else if (
+      window.location.pathname.includes("rainydays") ||
+      window.location.pathname.includes("vitrales") ||
+      window.location.pathname.includes("sgphotography") ||
+      window.location.pathname.includes("liverock") ||
+      window.location.pathname.includes("csmuseum") ||
+      window.location.pathname.includes("barklife")
+    ) {
+      navbarColour.style.backgroundColor = "#c3cbc9";
+    } else {
+      navbarColour.style.backgroundColor = "#e8e8e8";
+    }
+  }
 
   function toggleMenu() {
+    setNavColour();
     if (
-      hamburgerMenu.style.transform === "translateX(0%)" &&
+      navbar.style.transform === "translateX(0%)" &&
       window.innerWidth < 785
     ) {
-      hamburgerMenu.style.transform = "translateX(-100%)";
+      navbar.style.transform = "translateX(-100%)";
     } else {
-      hamburgerMenu.style.transform = "translateX(0%)";
+      navbar.style.transform = "translateX(0%)";
     }
   }
 
   window.onscroll = closeHamburgerNav();
 
   function closeHamburgerNav() {
-    if ((hamburgerMenu.style.transform = "translateX(0%)")) {
-      hamburgerMenu.style.transform = "translateX(-100%)";
+    if ((navbar.style.transform = "translateX(0%)")) {
+      navbar.style.transform = "translateX(-100%)";
     }
   }
 
-  const menuButtons = document.querySelectorAll(".hamburgerNav a");
+  const menuButtons = document.querySelectorAll(".navbar a");
 
   menuButtons.forEach((link) => {
     link.onclick = toggleMenu();
